d721ac66eb0cbc9bc7bb79f11be86c553eb89aa6
Aman Dinesh <xarmaaaruti@gmail.com>
2022-03-20 09:43:00 +0530

    End of Section 2

---
+++ API/Data/Migrations/20220319124724_UpdateCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UpdateCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityStock",
+                table: "Products",
+                newName: "QuantityInStock");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityStock");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
